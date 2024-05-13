8c450535f58cb22275d70fac69c1b0427922a588
armandonum <armandonunez404@gmail.com>
2024-05-13 18:52:17 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.NetworkInformation;
+//Console.WriteLine("Hello, World oop!");
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+      //  Console.WriteLine("Hello World!");
+
+
+        welcome welcome = new welcome();
+        welcome.show();
+
+    }
+}
---
+++ welcome.cs
@@ -0,0 +1,16 @@
+using System.Net.NetworkInformation;
+
+class welcome
+{
+
+    // Main method begins execution of C# application
+    public welcome()
+    {
+        System.Console.WriteLine("Welcome to C# Programming, good nice !");
+    }
+    public void show()
+    {
+        System.Console.WriteLine("Welcome to C# Programming!");
+    }
+ 
+}
