bbc4d83c2ef9cebe42f7a3d6d642271e521cf228
armandonum <armandonunez404@gmail.com>
2024-05-30 23:13:11 -0400

    add id to each game

--- hangegGame-ApiRest/obj/Debug/net8.0/hangegGame-ApiRest.AssemblyInfo.cs
+++ hangegGame-ApiRest/obj/Debug/net8.0/hangegGame-ApiRest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hangegGame-ApiRest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e7cc45e18ddc33fce6e6e9da8682fcebeaaa7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39796dd10f186fd9aa89d0333181656640bcbe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("hangegGame-ApiRest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hangegGame-ApiRest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
